54f5c90218dce39c8f6228590f6bd8077ee5b8eb
Elly <ellia.casey@gmail.com>
2024-10-13 14:27:52 -0600

    entries do not show up in current notebook, they go straight where they belong

--- client/src/services/EntriesService.js
+++ client/src/services/EntriesService.js
@@ -5,24 +5,26 @@ import { AppState } from "@/AppState.js"
 
 
 class EntriesService {
-  async createEntry(entryData) {
-    const response = await api.post('api/entries', entryData)
-    logger.log('new entry was created', response.data)
-    const createdEntry = new Entry(response.data)
-    AppState.entries.unshift(createdEntry)
-    return createdEntry
-  }
   async getEntriesById(notebookId) {
     const response = await api.get(`api/notebooks/${notebookId}/entries`)
-      logger.log('entries received by id', response.data)
-      const notebookEntries = response.data.map(entryData => new Entry(entryData))
-      AppState.entries = notebookEntries
+    logger.log('entries received by id', response.data)
+    const notebookEntries = response.data.map(entryData => new Entry(entryData))
+    AppState.entries = notebookEntries
   }
   async getUserEntries() {
     const response = await api.get('api/entries')
     logger.log('got user entries', response.data)
     const newEntries = response.data.map(entryData => new Entry(entryData))
     AppState.entries = newEntries
+  }
+  async createEntry(entryData) {
+    const response = await api.post('api/entries', entryData)
+    logger.log('new entry was created', response.data)
+    const createdEntry = new Entry(response.data)
+    const activeNotebook = AppState.activeNotebook
+    if (activeNotebook.id == createdEntry.notebookId)
+      AppState.entries.unshift(createdEntry)
+      return createdEntry
   }
   async updateEntry(entryData) {
       const response = await api.put(`api/entries/${entryData.id}`, entryData)
